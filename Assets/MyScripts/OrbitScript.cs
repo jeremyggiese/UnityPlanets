92697398d53fbb693060eb42150e847986ad8477
Jeremy Giese <giese@live.marshall.edu>
2020-03-03 16:39:59 -0500

    First Commit

---
+++ Assets/MyScripts/OrbitScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitScript : MonoBehaviour
+{
+     private Transform center;
+     public Vector3 axis = Vector3.up;
+     public Vector3 desiredPosition;
+     public float radius = 2.0f;
+     public float radiusSpeed = 0.5f;
+     public float rotationSpeed = 80.0f;
+     public GameObject centerObject;
+ 
+     void Start () {
+         center = centerObject.transform;
+         transform.position = (transform.position - center.position).normalized * radius + center.position;
+         radius = 2.0f;
+     }
+     
+     void Update () {
+         transform.RotateAround (center.position, axis, rotationSpeed * Time.deltaTime);
+         desiredPosition = (transform.position - center.position).normalized * radius + center.position;
+         transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * radiusSpeed);
+     }
+
+}
---
+++ Assets/MyScripts/WallScript.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallScript : MonoBehaviour
+{
+    public GameObject start, end;
+    private GameObject wall;
+    public GameObject wallFence;
+    private static bool creating;
+    public Camera camera;
+
+    void Start()
+    {
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        getInput();
+    }
+    private void getInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            setStart();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            setEnd();
+        }
+        else
+        {
+            if (creating)
+            {
+                adjust();
+            }
+        }
+    }
+    private void setStart()
+    {
+        creating = true;
+        GameObject startPoint = Instantiate(start, getWorldPoint(), Quaternion.Euler(0,0,0));
+        startPoint.active = true;
+
+    }
+    private void adjust()
+    {
+
+    }
+    private void setEnd()
+    {
+
+        GameObject endPoint = Instantiate(end, getWorldPoint(), Quaternion.Euler(0, 0, 0));
+        //end.transform.localScale = new Vector3(0.5f, 5, 0.5f);
+        endPoint.active = true;
+        //end.transform.position = getWorldPoint();
+        creating = false;
+    }
+    public Vector3 getWorldPoint()
+    { 
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            return hit.point;
+        }
+        else
+        {
+            return Vector3.zero;
+        }
+
+    }
+}
